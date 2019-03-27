2ba9f0e7ce82e0b132268a6699fefd88da970b98
bfconsidine <44121014+bfconsidine@users.noreply.github.com>
2019-03-26 20:52:11 -0400

    Update StaticManager.py

--- StaticManager.py
+++ StaticManager.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 '''
 IMMUTABLE. DO NOT CHANGE UNLESS YOU WANT THINGS TO BREAK.
@@ -6,12 +7,9 @@ THANKS, THE MANAGEMENT
 '''
 
 # configuration file statics
-CONFIG_FILE_NAME = "config.txt"
+CONFIG_FILE_NAME = "config.json"
 CONFIG_FILE_PATH = "" # assume local?
 
-# data acquisition statics
-V_DIVIDER_RESISTOR_GROUND = 0
-V_DIVIDER_RESISTOR_BRANCH = 0
 
 '''
 Config file parameters list (ADD NEW PARAMETERS AS DICTIONARY ENTRIES)
@@ -22,28 +20,28 @@ DEFAULT_CONFIG_PARAM = {
     # EDS default testing
     'SCHEDS1':[[1,-3],[1,-2],[1,-1]],
     'SCHEDS2':[[1,-3],[1,-2]],
-    'SCHEDS3':[[1,-3]],
-    'SCHEDS4':[[2,-3]],
-    'SCHEDS5':[[3,-3]],
+    'SCHEDS3':[[1,-2]],
+    'SCHEDS4':[[2,-2]],
+    'SCHEDS5':[[3,-2]],
     'SCHEDS6':[[1,0]],
-    'EDS1':27,
-    'EDS2':29,
-    'EDS3':31,
-    'EDS4':33,
-    'EDS5':35,
-    'EDS6':37,
-    'EDS1PV':8,
-    'EDS2PV':10,
+    'EDS1':4,
+    'EDS2':17,
+    'EDS3':6,
+    'EDS4':19,
+    'EDS5':26,
+    'EDS6':27,
+    'EDS1PV':14,
+    'EDS2PV':18,
     'EDS3PV':12,
     'EDS4PV':16,
-    'EDS5PV':18,
-    'EDSIDS':[[1],[2],[3],[4],[5]],
-    'CTRL1PV':26,
-    'CTRL2PV':28,
-    'CTRLIDS':[[1],[2]],
+    'EDS5PV':20,
+    'EDSIDS':[1,2,3,4,5],
+    'CTRL1PV':15,
+    'CTRL2PV':23,
+    'CTRLIDS':[1,2],
     'ADC':22,
-    'POWER':32,
-    'BATTERY':32,
+    'POWER':24,
+    'BATTERY':25,
     
     # testing requirements
     'maxTemperatureCelsius':40,
@@ -52,11 +50,13 @@ DEFAULT_CONFIG_PARAM = {
     'minRelativeHumidity':30,
     'testDurationSeconds':120,
     'testWindowSeconds':2700,
+    'voltDivResGround':0,
+    'voltDivResBranch':0,
     
     # indicators/switches
-    'outPinLEDGreen':0,
-    'outPinLEDRed':0,
-    'inPinManualActivate':4,
+    'outPinLEDGreen':5,
+    'outPinLEDRed':13,
+    'inPinManualActivate':8,
     'manualEDSNumber':1,
     'solarChargerEDSNumber':6,
     
@@ -76,73 +76,48 @@ Functionality:
 '''
 
 class StaticMaster:
-
+    
     def __init__(self):
         # immutable constants are not put in the dictionary
         self.config_name = CONFIG_FILE_NAME
         self.config_path = CONFIG_FILE_PATH
-        self.divider_res_grd = V_DIVIDER_RESISTOR_GROUND
-        self.divider_res_branch = V_DIVIDER_RESISTOR_BRANCH
-
-        self.config_dictionary = {} # sets up empty dictionary
-
+        
+        self.config_dictionary = {} # set up empty dictionary
+        
         if self.check_for_config():
             self.load_config()
             self.check_parameters()
         else:
             print("Configuration file not found! Creating default file...")
-            #lgf.logger.warning("Config file not found, default config file generated")
             self.create_default_config()
             self.load_config()
-
-
+            
+    
     def check_for_config(self):
         # checks for pre-existing config file in specified directory
         return os.path.isfile(self.config_path+self.config_name)
     
     
     def create_default_config(self):
-        # create default config file with parameters
-        try:
-            with open(self.config_name, 'w') as cfd:
-                for key in DEFAULT_CONFIG_PARAM:
-                    cfd.writelines(str(key)+"="+str(DEFAULT_CONFIG_PARAM[key])+'\n')
-        except RuntimeError:
-            print("Error creating default configuration file!")
-
+        # create config file with default parameters
+        with open(CONFIG_FILE_NAME, 'w') as cf:
+            json.dump(DEFAULT_CONFIG_PARAM, cf)
+            
+            
     def load_config(self):
-        # loads configuration file constants if file exists
-        lines = []
-
+        # load config file if already exists
         try:
-            with open(self.config_path+self.config_name) as cf:
-                lines = cf.read().split('\n')
-        except RuntimeError:
-            print("Error reading configuration file. Generating default config...")
+            with open(CONFIG_FILE_NAME, 'r') as cf:
+                self.config_dictionary = json.load(cf)
+        except:
+            # remove faulty file if it can't be loaded
+            print("Error loading configuration file. Deleting and remaking with default parameters!")
+            if os.path.isfile(self.config_path+self.config_name):
+                os.remove(self.config_path+self.config_name)
+            # create default file after deleting corrupted file
             self.create_default_config()
-            self.load_config()
-
-        # loading individual config values into config dictionary
-        if lines:
-            for c_string in lines:
-                if c_string != '':
-                    try:
-                        self.config_dictionary[c_string.split('=')[0]] = c_string.split('=')[1]
-                    except:
-                        print("Configuration format not correct. Generating default config...")
-                        self.create_default_config()
-                        self.load_config()
-                        
-                '''
-                This is the most concise way of doing what we want here. Each dictionary key will then 
-                just be the first string of each config file parameter.
-                For example, self.config_dictionary['maxRelativeHumidity'] accesses the config value.
-                '''
-        else:
-            print("Configuration file empty! Generating default config...")
-            self.create_default_config()
-            self.load_config()
-
+            
+            
     def check_parameters(self):
         # checks to make sure all parameters exist in self dictionary after loading from config
         for key in DEFAULT_CONFIG_PARAM:
@@ -151,28 +126,12 @@ class StaticMaster:
                 self.config_dictionary[key] = DEFAULT_CONFIG_PARAM[key]
                 print("Adding parameter ["+str(key)+"] to dictionary.\n")
                 # then add parameter with default value to config file (CONFIG MUST EXIST)
-                with open(self.config_path+self.config_name, 'a') as cf:
-                    cf_data = str(key)+"="+str(DEFAULT_CONFIG_PARAM[key])+'\n'
-                    cf.write(cf_data)
-
-
+                with open(self.config_path+self.config_name, 'w') as cf:
+                    json.dump(self.config_dictionary, cf)
+                    
+                    
     def get_config(self):
         # returns config dictionary for other functions to use
         return self.config_dictionary
-    
-    
-    def write_reboot_flag(self, check, flag):
-        # loads config file then changes reboot flag if currently false
-        self.load_config()
-        if self.config_dictionary["rebootFlag"] == check:
-            if self.check_for_config():
-                # write reboot flag as True in config file if not already true
-                with open(self.config_path+self.config_name, 'r') as cf:
-                    cf_data = cf.read()
-
-                cf_data = cf_data.replace('rebootFlag='+check, 'rebootFlag='+flag)
-
-                with open(self.config_path+self.config_name, 'w') as cf:
-                    cf.write(cf_data)
 
 
