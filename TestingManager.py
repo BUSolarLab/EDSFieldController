ba9eaf31598dd57b2b04e5ef464395c06a7154f3
bfconsidine <44121014+bfconsidine@users.noreply.github.com>
2019-03-26 20:51:34 -0400

    Update TestingManager.py

--- TestingManager.py
+++ TestingManager.py
@@ -12,9 +12,14 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 ADC_EDS_CHAN = MCP.P0
 ADC_BAT_CHAN = MCP.P1
 
+# year days for start of each month (because the clock doesn't want to keep tm_yday for some reason)
+# don't care about leap year
+Y_DAYS = [0,31,59,90,120,151,181,212,243,273,304,334]
+
 # tolerances for humidity and temperature scheduling
 T_TOL = 0.1
 H_TOL = 0.1
+MIN_CHECK_THRESHOLD = 0.5
 
 # GPIO setup
 GPIO.cleanup()
@@ -93,22 +98,21 @@ class TestingMaster:
         
         
     # check time against schedule
-    def check_time(self, dt, solar_offset, eds_num):
+    def check_time(self, dt, yday, solar_offset, eds_num):
         sched_name = 'SCHEDS'+str(eds_num)
         schedule = self.test_config[sched_name]
-        print(schedule)
         for pair in schedule:
             # if mod of the year day with schedule is zero, then day is correct
-            if dt.tm_yday % float(pair[0]) == 0:
+            if yday % float(pair[0]) == 0:
                 # convert current time to minutes and add solar time offset
-                dt_min = curr.tm_hour * 60 + curr.tm_min + curr.tm_sec / 60
-                dt_min_solar = curr_min + solar_offset
+                dt_min = dt.tm_hour * 60 + dt.tm_min + dt.tm_sec / 60
+                dt_min_solar = dt_min + solar_offset
                 
                 # check if current minute is close enough to schedule time offset
                 # calculate schedule time in minutes
                 schedule_min = 720 + float(pair[1]) * 60
                 # if the time is within 30 seconds of scheduled time
-                if abs(dt_min_solar - schedule_min) < 0.5:
+                if abs(dt_min_solar - schedule_min) < MIN_CHECK_THRESHOLD:
                     return True
     
     # check weather against parameters
@@ -119,7 +123,7 @@ class TestingMaster:
         if ((1 - T_TOL) * t_low) <= t_curr <= (t_high * (1 + T_TOL)): # tolerance allows a bit outside range
             return True
         else:
-            print("Temperature not within testing parameters. Will check again shortly.")
+            print("Temperature (", t_curr, " C) not within testing parameters. Will check again shortly.")
             return False
         
     def check_humid(self, h_curr):
@@ -129,7 +133,7 @@ class TestingMaster:
         if ((1 - H_TOL) * h_low) <= h_curr <= (h_high * (1 + H_TOL)): # tolerance allows a bit outside range
             return True
         else:
-            print("Humidity not within testing parameters. Will check again shortly.")
+            print("Humidity (", h_curr, " %) not within testing parameters. Will check again shortly.")
             return False
 
     
@@ -137,16 +141,12 @@ class TestingMaster:
     def run_test(self, eds_num):
         #  main test sequence to be run after checking flags in MasterManager
         test_duration = self.get_param('testDurationSeconds')
-        
         # run first half of test
         self.run_test_begin(eds_num)
-        
         # wait for test duration
         time.sleep(test_duration)
-        
         # run second half of test
         self.run_test_end(eds_num)
-        
         
     def run_measure_EDS(self, eds_num):
         
@@ -224,9 +224,7 @@ class TestingMaster:
         # 1) EDS activation relays ON
         time.sleep(0.5)
         GPIO.setup(eds_select, GPIO.OUT)
-        
         time.sleep(0.5) # short delay between relay switching
-        
         # 2) power supply relay ON
         GPIO.setup(ps_relay, GPIO.OUT)
         
@@ -238,9 +236,22 @@ class TestingMaster:
         # 4) power supply relay OFF
         GPIO.cleanup(ps_relay)
         time.sleep(0.5)
-        
         # 5) EDS activation relays OFF
         GPIO.cleanup(eds_select)
         time.sleep(0.5)
         
 
+            
+    
+            
+            
+            
+            
+
+
+        
+        
+        
+        
+        
+        
