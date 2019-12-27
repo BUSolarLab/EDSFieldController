9d2bf519d053ea5fc02ff75596af53c9222b8e30
adwikara <adityawikara@gmail.com>
2019-12-26 23:18:28 -0500

    usb master

--- testing_script/usb_test.py
+++ testing_script/usb_test.py
@@ -58,7 +58,6 @@ class USBMaster:
             f.close()
             self.set_mounting_port()
 
-
     def set_USB_path(self):
         # gets USB file path for saving if USB name found
         if self.USB_name is not None:
