5597bc202bc669e09c1357ff34cd1d11507eb48a
Aditya Wikara <adityawikara@gmail.com>
2020-03-05 11:52:11 -0500

    debugging

--- MasterManager.py
+++ MasterManager.py
@@ -294,7 +294,6 @@ while True:
                 print_l(curr_dt, "POST EDS Solar Noon SR for " + panel + ": " + str(sr_post))
                 data[panel]['sr_post'] = sr_post
                 # write data to csv file
-                usb_master.check_USB()
                 csv_master.write_noon_data(data[panel])
                 print_l(curr_dt, "Writing Noon Mode Measurements Results To CSV and TXT Files")
                 # delay before changing to next EDS panel
