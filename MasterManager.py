2b28f4e51a2b8910a363f062c64956ca6d9539c9
adwikara <adityawikara@gmail.com>
2020-03-13 14:53:56 -0400

    fix scheduling

--- MasterManager.py
+++ MasterManager.py
@@ -147,10 +147,11 @@ while True:
         Checking if RTC is working (initial check)
         --------------------------------------------------------------------------
         '''
+        current_time = rtc.datetime
         #solar_offset = ceil(DM.get_solar_time(gmt_offset,longitude, rtc.datetime) * 100)/100
         try:
             current_time = rtc.datetime
-            #solar_offset = ceil(DM.get_solar_time(gmt_offset, current_time, longitude, latitude) * 100)/100
+            solar_offset = ceil(DM.get_solar_time(gmt_offset, current_time, longitude, latitude) * 100)/100
             
             # remove error if corrected
             if "Sensor-RTC-1" in error_list:
