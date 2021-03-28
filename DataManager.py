800825fc99673cba22a7e457ac21fae3316518ba
BUSolarLab <bmahabir@bu.edu>
2021-03-28 07:33:15 -0400

    .

--- DataManager.py
+++ DataManager.py
@@ -156,15 +156,14 @@ class USBMaster:
         subprocess.call("sudo chown -R pi:pi /media/"+str(self.label), shell=True)
         #unmount if already mounted wait then mount to make sure its writable on mount
         subprocess.call("sudo umount /dev/sda1", shell=True)
-        time.sleep(0.5)
+        time.sleep(0.1)
         subprocess.call("sudo mount /dev/sda1 /media/"+str(self.label)+" -o uid=pi,gid=pi", shell=True)
 
     # un-mount all USBs
     def reset_usb_mounts(self):
         print("Un-Mounting USB")
-        time.sleep(0.5)
-        #subprocess.call("sudo umount /media/"+str(self.label), shell=True)
-        subprocess.call("sudo umount /dev/sda1", shell=True)
+        time.sleep(0.1)
+        subprocess.call("sudo umount /media/"+str(self.label), shell=True)
 
     # edit fstab file to auto-mount when boot
     def update_fstab_file(self):
